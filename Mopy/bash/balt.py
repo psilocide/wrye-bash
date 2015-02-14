a8780fde7b0d10257c1bb3817274678154110f1d
MrD <the.ubik@gmail.com>
2015-02-14 19:58:11 +0100

    defaultSashPos = minimumSize = 256, round and common - _sizeHints binned:RRR
    Under 163.
    
    To see the sizeHints in action unbind the OnSize() callback in UIList.
    UIList displays in size hints size IIUC.
    Sizes/EVENT_SIZE handling needs reworking from the ground up - for now
    I just make the UI more consistent (one of the first rules of UI design).
    Plus those are really class level attributes - yes, those are all
    essentially singleton classes (the UILists).
    minimumSize sets the panels isVertical ? width : height. Maybe 256 is a
    lot but installers was more (the sash pos was at 530) plus the UI design
    is post refactoring concern.

--- Mopy/bash/balt.py
+++ Mopy/bash/balt.py
@@ -1719,7 +1719,6 @@ class ListCtrl(wx.ListCtrl, ListCtrlAutoWidthMixin):
 #------------------------------------------------------------------------------
 class UIList(wx.Panel):
     """Tmp class to factor out common code in basher.List and balt.Tank."""
-    _sizeHints = (100, 100) # min ListCtrl size TODO(ut): random overrides
     # optional menus
     mainMenu = None
     itemMenu = None
@@ -1739,7 +1738,6 @@ class UIList(wx.Panel):
         #--Layout
         sizer = vSizer()
         self.SetSizer(sizer)
-        self.SetSizeHints(*self.__class__._sizeHints)
         # Settings key
         self.__class__.keyPrefix = keyPrefix
         #--Columns

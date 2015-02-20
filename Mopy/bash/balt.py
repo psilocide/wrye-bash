psilocide/wrye-bash

commit eba53f93bea72445a9fdac56c82172baeb2ab837

Installer item menus fixup - enough with data:
Installer item menus were broken as _append tried to read non existent data
attribute - set by _initData in AppendToMenu.
Introduced idata for the (singleton) data source (good old
bosh.InstallersData) - goal: unroot self.data from Link classes
filterInstallables(): why the isinstance check AND the type in (1,2) check ?

Mopy/bash/basher/__init__.py
@@ -68,7 +68,7 @@ from ..balt import tooltip, fill, bell
 from ..balt import bitmapButton, button, toggleButton, checkBox, staticText, spinCtrl, textCtrl
 from ..balt import spacer, hSizer, vSizer, hsbSizer
 from ..balt import colors, images, Image
-from ..balt import Links, ListCtrl
+from ..balt import Links, ListCtrl, _Link
 
 # BAIN wizard support, requires PyWin32, so import will fail if it's not installed
 try:
@@ -108,9 +108,39 @@ gMessageList = None
 bashFrame = None
 docBrowser = None
 modChecker = None
+gPeople = None # New global - yak
 
 # Settings --------------------------------------------------------------------
 settings = None
+
+# Links -----------------------------------------------------------------
+#------------------------------------------------------------------------------
+def SetUAC(item):
+    """Helper function for creating menu items or buttons that need UAC
+       Note: for this to work correctly, it needs to be run BEFORE
+       appending a menu item to a menu (and so, needs to be enabled/
+       diasbled prior to that as well."""
+    if isUAC:
+        if isinstance(item,wx.MenuItem):
+            pass
+            #if item.IsEnabled():
+            #    bitmap = images['uac.small'].GetBitmap()
+            #    item.SetBitmaps(bitmap,bitmap)
+        else:
+            balt.setUAC(item,isUAC)
+
+# Tank link mixins to access the Tank data - not final
+class Installers_Link(_Link):
+    """InstallersData mixin"""
+
+    @property
+    def idata(self): return gInstallers.data # InstallersData singleton
+
+class People_Link(_Link):
+    """PeopleData mixin"""
+
+    @property
+    def pdata(self): return gPeople.data # PeopleData singleton
 
 # Constants --------------------------------------------------------------------
 from .constants import colorInfo, tabInfo, settingDefaults, karmacons, \
@@ -4382,6 +4412,8 @@ class PeoplePanel(SashTankPanel):
 
     def __init__(self,parent):
         """Initialize."""
+        global gPeople
+        gPeople = self
         data = bosh.PeopleData()
         SashTankPanel.__init__(self,data,parent)
         left,right = self.left,self.right
@@ -5226,8 +5258,6 @@ class BashFrame(wx.Frame):
                     path.remove()
 
 #------------------------------------------------------------------------------
-from ..balt import _Link
-
 class CheckList_SelectAll(_Link):
     def __init__(self,select=True):
         super(CheckList_SelectAll, self).__init__()
@@ -6667,22 +6697,6 @@ class CreateNewProject(wx.Dialog):
         gInstallers.refreshed = False
         gInstallers.fullRefresh = self.fullRefresh
         gInstallers.OnShow()
-
-# Links -----------------------------------------------------------------
-#------------------------------------------------------------------------------
-def SetUAC(item):
-    """Helper function for creating menu items or buttons that need UAC
-       Note: for this to work correctly, it needs to be run BEFORE
-       appending a menu item to a menu (and so, needs to be enabled/
-       diasbled prior to that as well."""
-    if isUAC:
-        if isinstance(item,wx.MenuItem):
-            pass
-            #if item.IsEnabled():
-            #    bitmap = images['uac.small'].GetBitmap()
-            #    item.SetBitmaps(bitmap,bitmap)
-        else:
-            balt.setUAC(item,isUAC)
 
 # Initialization --------------------------------------------------------------
 from .gui_patchers import initPatchers

Mopy/bash/basher/misc_links.py
@@ -27,6 +27,7 @@ import wx
 from ..balt import EnabledLink, AppendableLink, _Link, Link, RadioLink, \
     ChoiceLink, MenuLink, CheckLink
 from .. import balt, bosh, bush
+from .import People_Link
 from .constants import ID_GROUPS, settingDefaults, tabInfo
 from ..bolt import GPath, LString
 
@@ -185,7 +186,7 @@ class Message_Delete(_Link):
 
 # People Links ----------------------------------------------------------------
 #------------------------------------------------------------------------------
-class People_AddNew(_Link):
+class People_AddNew(People_Link):
     """Add a new record."""
     dialogTitle = _(u'Add New Person')
     text = _(u'Add...')
@@ -194,16 +195,16 @@ class People_AddNew(_Link):
     def Execute(self,event):
         name = balt.askText(self.gTank,_(u"Add new person:"),self.dialogTitle)
         if not name: return
-        if name in self.data:
+        if name in self.pdata:
             return balt.showInfo(self.gTank, name + _(u" already exists."),
                                  self.dialogTitle)
-        self.data[name] = (time.time(),0,u'')
+        self.pdata[name] = (time.time(),0,u'')
         self.gTank.RefreshUI(details=name)
         self.gTank.gList.EnsureVisible(self.gTank.GetIndex(name))
-        self.data.setChanged()
+        self.pdata.setChanged()
 
 #------------------------------------------------------------------------------
-class People_Export(_Link):
+class People_Export(People_Link):
     """Export people to text archive."""
     dialogTitle = _(u"Export People")
     text = _(u'Export...')
@@ -216,13 +217,13 @@ class People_Export(_Link):
                             textDir, u'People.txt', u'*.txt')
         if not path: return
         bosh.settings['bash.workDir'] = path.head
-        self.data.dumpText(path,self.selected)
+        self.pdata.dumpText(path,self.selected)
         balt.showInfo(self.gTank,
                       _(u'Records exported: %d.') % len(self.selected),
                       self.dialogTitle)
 
 #------------------------------------------------------------------------------
-class People_Import(_Link):
+class People_Import(People_Link):
     """Import people from text archive."""
     dialogTitle = _(u"Import People")
     text = _(u'Import...')
@@ -235,13 +236,13 @@ class People_Import(_Link):
                             textDir, u'', u'*.txt', mustExist=True)
         if not path: return
         bosh.settings['bash.workDir'] = path.head
-        newNames = self.data.loadText(path)
+        newNames = self.pdata.loadText(path)
         balt.showInfo(self.gTank, _(u"People imported: %d") % len(newNames),
                       self.dialogTitle)
         self.gTank.RefreshUI()
 
 #------------------------------------------------------------------------------
-class People_Karma(ChoiceLink):
+class People_Karma(People_Link, ChoiceLink):
     """Add Karma setting links."""
     text = _(u'Karma')
     idList = ID_GROUPS
@@ -255,10 +256,10 @@ class People_Karma(ChoiceLink):
         idList = ID_GROUPS
         karma = range(5,-6,-1)[event.GetId()-idList.BASE]
         for item in self.selected:
-            text = self.data[item][2]
-            self.data[item] = (time.time(),karma,text)
+            text = self.pdata[item][2]
+            self.pdata[item] = (time.time(),karma,text)
         self.gTank.RefreshUI()
-        self.data.setChanged()
+        self.pdata.setChanged()
 
 # Masters Links ---------------------------------------------------------------
 #------------------------------------------------------------------------------

Mopy/bash/basher/installers_links.py
@@ -23,8 +23,8 @@
 # =============================================================================
 import copy
 from .. import bosh, balt, bush
-from ..balt import fill, _Link, BoolLink, AppendableLink, Link, EnabledLink
-from . import ListBoxes, CreateNewProject, Resources
+from ..balt import fill, BoolLink, AppendableLink, Link
+from . import ListBoxes, CreateNewProject, Resources, Installers_Link
 from ..bolt import GPath, SubProgress
 
 gInstallers = None
@@ -32,7 +32,7 @@ gInstallers = None
 #------------------------------------------------------------------------------
 # Installers Links ------------------------------------------------------------
 #------------------------------------------------------------------------------
-class Installers_AddMarker(_Link):
+class Installers_AddMarker(Installers_Link):
     """Add an installer marker."""
     text = _(u'Add Marker...')
     help = _(u'Adds a Marker, a special type of package useful for separating'
@@ -42,8 +42,8 @@ class Installers_AddMarker(_Link):
         """Handle selection."""
         index = self.gTank.GetIndex(GPath(u'===='))
         if index == -1:
-            self.data.addMarker(u'====')
-            self.data.refresh(what='OS')
+            self.idata.addMarker(u'====')
+            self.idata.refresh(what='OS')
             gInstallers.RefreshUIMods()
             index = self.gTank.GetIndex(GPath(u'===='))
         if index != -1:
@@ -51,7 +51,7 @@ class Installers_AddMarker(_Link):
             self.gTank.SelectItemAtIndex(index)
             self.gTank.gList.EditLabel(index)
 
-class Installers_MonitorInstall(_Link):
+class Installers_MonitorInstall(Installers_Link):
     """Monitors Data folder for external installation."""
     text = _(u'Monitor External Installation...')
     help = _(u'Monitors the Data folder during installation via manual install'
@@ -167,13 +167,12 @@ class Installers_MonitorInstall(_Link):
         gInstallers.OnShow()
         # Update the status of the installer (as installer last)
         path = path.relpath(bosh.dirs['installers'])
-        self.data.install([path],None,True,False)
+        self.idata.install([path],None,True,False)
         # Refresh UI
         gInstallers.RefreshUIMods()
         # Select new installer
         self.gTank.SelectItemAtIndex(self.gTank.gList.GetItemCount()-1)
-
-class Installers_ListPackages(EnabledLink):
+class Installers_ListPackages(Installers_Link):
     """Copies list of Bain files to clipboard."""
     text = _(u'List Packages...')
     help = _(u'Displays a list of all packages.  Also copies that list to the '
@@ -186,12 +185,12 @@ class Installers_ListPackages(EnabledLink):
                    _(u'(Else shows all packages)')
                    )
         if balt.askYes(self.gTank,message,_(u'Only Show Installed?')):
-            text = self.data.getPackageList(False)
-        else: text = self.data.getPackageList()
+            text = self.idata.getPackageList(False)
+        else: text = self.idata.getPackageList()
         balt.copyToClipboard(text)
         balt.showLog(self.gTank,text,_(u'BAIN Packages'),asDialog=False,fixedFont=False,icons=Resources.bashBlue)
 
-class Installers_AnnealAll(_Link):
+class Installers_AnnealAll(Installers_Link):
     """Anneal all packages."""
     text = _(u'Anneal All')
     help = _(u'This will install any missing files (for active installers)'
@@ -201,13 +200,13 @@ class Installers_AnnealAll(_Link):
         """Handle selection."""
         try:
             with balt.Progress(_(u"Annealing..."),u'\n'+u' '*60) as progress:
-                self.data.anneal(progress=progress)
+                self.idata.anneal(progress=progress)
         finally:
-            self.data.refresh(what='NS')
+            self.idata.refresh(what='NS')
             gInstallers.RefreshUIMods()
             Link.Frame.RefreshData()
 
-class Installers_UninstallAllPackages(_Link):
+class Installers_UninstallAllPackages(Installers_Link):
     """Uninstall all packages."""
     text = _(u'Uninstall All Packages')
     help = _(u'This will uninstall all packages.')
@@ -217,13 +216,13 @@ class Installers_UninstallAllPackages(_Link):
         if not balt.askYes(self.gTank,fill(_(u"Really uninstall All Packages?"),70),self.text): return
         try:
             with balt.Progress(_(u"Uninstalling..."),u'\n'+u' '*60) as progress:
-                self.data.uninstall(unArchives='ALL',progress=progress)
+                self.idata.uninstall(unArchives='ALL',progress=progress)
         finally:
-            self.data.refresh(what='NS')
+            self.idata.refresh(what='NS')
             gInstallers.RefreshUIMods()
             Link.Frame.RefreshData()
 
-class Installers_Refresh(AppendableLink, _Link):
+class Installers_Refresh(AppendableLink, Installers_Link):
     """Refreshes all Installers data."""
     def __init__(self, fullRefresh=False):
         super(Installers_Refresh, self).__init__()
@@ -245,7 +244,7 @@ class Installers_Refresh(AppendableLink, _Link):
         gInstallers.fullRefresh = self.fullRefresh
         gInstallers.OnShow()
 
-class Installers_UninstallAllUnknownFiles(_Link):
+class Installers_UninstallAllUnknownFiles(Installers_Link):
     """Uninstall all files that do not come from a current package/bethesda
     files. For safety just moved to Oblivion Mods\Bash Installers\Bash\Data
     Folder Contents (date/time)\."""
@@ -267,26 +266,26 @@ class Installers_UninstallAllUnknownFiles(_Link):
             try:
                 with balt.Progress(_(u"Cleaning Data Files..."),
                                    u'\n' + u' ' * 65) as progress:
-                    self.data.clean(progress=progress)
+                    self.idata.clean(progress=progress)
             finally:
-                self.data.refresh(what='NS')
+                self.idata.refresh(what='NS')
                 gInstallers.RefreshUIMods()
                 Link.Frame.RefreshData()
 
 #------------------------------------------------------------------------------
 # Installers BoolLinks --------------------------------------------------------
 #------------------------------------------------------------------------------
-class Installers_AutoAnneal(BoolLink):
+class Installers_AutoAnneal(Installers_Link, BoolLink):
     text, key, help = _(u'Auto-Anneal'), 'bash.installers.autoAnneal', _(
         u"Enable/Disable automatic annealing of packages.")
 
-class Installers_AutoWizard(BoolLink):
+class Installers_AutoWizard(Installers_Link, BoolLink):
     text = _(u'Auto-Anneal/Install Wizards')
     key = 'bash.installers.autoWizard'
     help = _(u"Enable/Disable automatic installing or anneal (as applicable)"
              u" of packages after running its wizard.")
 
-class _Installers_BoolLink_Refresh(BoolLink):
+class _Installers_BoolLink_Refresh(Installers_Link, BoolLink):
     def Execute(self,event):
         super(_Installers_BoolLink_Refresh, self).Execute(event)
         gInstallers.gList.RefreshUI()
@@ -298,12 +297,12 @@ class Installers_WizardOverlay(_Installers_BoolLink_Refresh):
     help =_(u"Enable/Disable the magic wand icon overlay for packages with"
             u" Wizards.")
 
-class Installers_AutoRefreshProjects(BoolLink):
+class Installers_AutoRefreshProjects(Installers_Link, BoolLink):
     """Toggle autoRefreshProjects setting and update."""
     text = _(u'Auto-Refresh Projects')
     key = 'bash.installers.autoRefreshProjects'
 
-class Installers_AutoApplyEmbeddedBCFs(BoolLink):
+class Installers_AutoApplyEmbeddedBCFs(Installers_Link, BoolLink):
     """Toggle autoApplyEmbeddedBCFs setting and update."""
     text = _(u'Auto-Apply Embedded BCFs')
     key = 'bash.installers.autoApplyEmbeddedBCFs'
@@ -314,7 +313,7 @@ class Installers_AutoApplyEmbeddedBCFs(BoolLink):
         super(Installers_AutoApplyEmbeddedBCFs, self).Execute(event)
         gInstallers.OnShow()
 
-class Installers_AutoRefreshBethsoft(BoolLink):
+class Installers_AutoRefreshBethsoft(Installers_Link, BoolLink):
     """Toggle refreshVanilla setting and update."""
     text = _(u'Skip Bethsoft Content')
     key = 'bash.installers.autoRefreshBethsoft'
@@ -352,7 +351,7 @@ class Installers_AutoRefreshBethsoft(BoolLink):
             gInstallers.data.refresh(what='NSC')
             gInstallers.gList.RefreshUI()
 
-class Installers_Enabled(BoolLink):
+class Installers_Enabled(Installers_Link, BoolLink):
     """Flips installer state."""
     text, key, help = _(u'Enabled'), 'bash.installers.enabled', _(
         u'Enable/Disable the Installers tab.')
@@ -376,7 +375,7 @@ class Installers_Enabled(BoolLink):
             gInstallers.gList.gList.DeleteAllItems()
             gInstallers.RefreshDetails(None)
 
-class Installers_BsaRedirection(AppendableLink, BoolLink):
+class Installers_BsaRedirection(AppendableLink, Installers_Link, BoolLink):
     """Toggle BSA Redirection."""
     text, key = _(u'BSA Redirection'),'bash.bsaRedirection',
 
@@ -410,17 +409,17 @@ class Installers_ConflictsReportShowBSAConflicts(_Installers_BoolLink_Refresh):
     text = _(u'Show BSA Conflicts')
     key = 'bash.installers.conflictsReport.showBSAConflicts'
 
-class Installers_AvoidOnStart(BoolLink):
+class Installers_AvoidOnStart(Installers_Link, BoolLink):
     """Ensures faster bash startup by preventing Installers from being startup tab."""
     text, key, help = _(u'Avoid at Startup'), 'bash.installers.fastStart', _(
         u"Toggles Wrye Bash to avoid the Installers tab on startup,"
         u" avoiding unnecessary data scanning.")
 
-class Installers_RemoveEmptyDirs(BoolLink):
+class Installers_RemoveEmptyDirs(Installers_Link, BoolLink):
     """Toggles option to remove empty directories on file scan."""
     text, key = _(u'Clean Data Directory'), 'bash.installers.removeEmptyDirs'
 
-class Installers_SortActive(BoolLink):
+class Installers_SortActive(Installers_Link, BoolLink):
     """Sort by type."""
     text, key, help = _(u'Sort by Active'), 'bash.installers.sortActive', _(
         u'If selected, active installers will be sorted to the top of the list.')
@@ -429,7 +428,7 @@ class Installers_SortActive(BoolLink):
         super(Installers_SortActive, self).Execute(event)
         self.gTank.SortItems()
 
-class Installers_SortProjects(BoolLink):
+class Installers_SortProjects(Installers_Link, BoolLink):
     """Sort dirs to the top."""
     text, key, help = _(u'Projects First'), 'bash.installers.sortProjects', _(
         u'If selected, projects will be sorted to the top of the list.')
@@ -438,7 +437,7 @@ class Installers_SortProjects(BoolLink):
         super(Installers_SortProjects, self).Execute(event)
         self.gTank.SortItems()
 
-class Installers_SortStructure(BoolLink):
+class Installers_SortStructure(Installers_Link, BoolLink):
     """Sort by type."""
     text, key = _(u'Sort by Structure'), 'bash.installers.sortStructure'
 
@@ -449,16 +448,16 @@ class Installers_SortStructure(BoolLink):
 #------------------------------------------------------------------------------
 # Installers_Skip Links -------------------------------------------------------
 #------------------------------------------------------------------------------
-class Installers_Skip(BoolLink):
+class Installers_Skip(Installers_Link, BoolLink):
     """Toggle various skip settings and update."""
     def Execute(self,event):
         super(Installers_Skip, self).Execute(event)
         with balt.Progress(_(u'Refreshing Packages...'),u'\n'+u' '*60, abort=False) as progress:
-            progress.setFull(len(self.data))
-            for index,dataItem in enumerate(self.data.iteritems()):
+            progress.setFull(len(self.idata))
+            for index,dataItem in enumerate(self.idata.iteritems()):
                 progress(index,_(u'Refreshing Packages...')+u'\n'+dataItem[0].s)
                 dataItem[1].refreshDataSizeCrc()
-        self.data.refresh(what='NS')
+        self.idata.refresh(what='NS')
         self.gTank.RefreshUI()
 
 class Installers_SkipScreenshots(Installers_Skip):
@@ -496,10 +495,8 @@ class Installers_SkipOBSEPlugins(AppendableLink, Installers_Skip):
     text = _(u'Skip %s Plugins') % bush.game.se_sd
     key = 'bash.installers.allowOBSEPlugins'
 
-    def __init__(self):
-        super(Installers_SkipOBSEPlugins, self).__init__(True)
-
     def _append(self, window): return bool(bush.game.se_sd)
+    def _check(self): return not bosh.settings[self.key]
 
 class Installers_RenameStrings(AppendableLink, Installers_Skip):
     """Toggle auto-renaming of .STRINGS files"""
@@ -508,7 +505,7 @@ class Installers_RenameStrings(AppendableLink, Installers_Skip):
 
     def _append(self, window): return bool(bush.game.esp.stringsFiles)
 
-class Installers_CreateNewProject(_Link):
+class Installers_CreateNewProject(Installers_Link):
     """Open the Create New Project Dialog"""
     text = _(u'Create New Project...')
     help = _(u'Create a new project...')

commit 7a5adf5c079b1034aaaad823884bd61fb895521a

Regression - Karma menu item raising AbstractError:
Introduced in eba53f93bea72445a9fdac56c82172baeb2ab837.
The PeopleLink.AppendToMenu() appended to menu the ChoiceLink which does
not override ItemLink.Execute():

Traceback (most recent call last):
  File "bash\balt.py", line 2375, in Execute
    raise AbstractError
bash.bolt.AbstractError: Abstract section called.

Reversing the order just appended the CL first instead of last - plus
the `subMenu = super(...)` call returned not wx.Menu but a wx.MenuItem:

Traceback (most recent call last):
  File "bash\balt.py", line 1836, in DoItemMenu
    self.itemMenu.PopupMenu(self,Link.Frame,selected)
  File "bash\balt.py", line 2293, in PopupMenu
    link.AppendToMenu(menu,parent,*args)
  File "bash\balt.py", line 2512, in AppendToMenu
    link.AppendToMenu(menu, window, data)
  File "bash\balt.py", line 2442, in AppendToMenu
    self.__class__.kind)
  File "C:\_\Python27\lib\site-packages\wx-2.8-msw-unicode\wx\_core.py", line 11490, in __init__
    _core_.MenuItem_swiginit(self,_core_.new_MenuItem(*args, **kwargs))
TypeError: in method 'new_MenuItem', expected argument 1 of type 'wxMenu *'

Solved by bringing People_Link closer to object - this resulted in
having to use MI with ItemLink in its subclasses.

Also People_Karma was made again to submenu. This was introduced in
6fbec32fbecbc72f72c5dd908e8d12a503fde502
but you can't see it till c84db2ca948a0011599ce0e52ddedbdb9714a29f
wheere People List Add... works again.

Consider those mixins deprecated.

Mopy/bash/balt.py
@@ -2433,8 +2433,9 @@ class ChoiceLink(Link):
 
     def AppendToMenu(self,menu,window,data):
         """Append idList items and register their callbacks."""
-        subMenu = super(ChoiceLink, self).AppendToMenu(menu, window, data)
-        if subMenu: menu = subMenu # our super is a MenuLink instance not mere Link instance
+        submenu = super(ChoiceLink, self).AppendToMenu(menu, window, data)
+        if isinstance(submenu, wx.Menu): # we inherit a Menu, append to it
+            menu = submenu
         for link in self.extraItems:
             link.AppendToMenu(menu, window, data)
         for link in self._range():
@@ -2442,7 +2443,7 @@ class ChoiceLink(Link):
         #--Events
         wx.EVT_MENU_RANGE(Link.Frame, self.idList.BASE, self.idList.MAX,
                           self.DoList)
-        # notice it returns None
+        # returns None
 
     def DoList(self, event): event.Skip()
 

Mopy/bash/basher/__init__.py
@@ -141,7 +141,7 @@ class Installers_Link(ItemLink):
     @property
     def idata(self): return gInstallers.data # InstallersData singleton
 
-class People_Link(ItemLink):
+class People_Link(Link):
     """PeopleData mixin"""
 
     @property

Mopy/bash/basher/links.py
@@ -831,7 +831,7 @@ def InitPeopleLinks():
     PeopleList.mainMenu.append(
         List_Columns('bash.people.cols', 'bash.people.allCols', ['Name']))
     #--Item links
-    PeopleList.itemMenu.append(People_Karma())
+    PeopleList.itemMenu.append(People_Karma(_(u'Karma')))
     PeopleList.itemMenu.append(SeparatorLink())
     PeopleList.itemMenu.append(People_AddNew())
     PeopleList.itemMenu.append(balt.Tank_Delete())

Mopy/bash/basher/misc_links.py
@@ -186,7 +186,7 @@ class Message_Delete(ItemLink):
 
 # People Links ----------------------------------------------------------------
 #------------------------------------------------------------------------------
-class People_AddNew(People_Link):
+class People_AddNew(ItemLink, People_Link):
     """Add a new record."""
     dialogTitle = _(u'Add New Person')
     text = _(u'Add...')
@@ -203,7 +203,7 @@ class People_AddNew(People_Link):
         self.pdata.setChanged()
 
 #------------------------------------------------------------------------------
-class People_Export(People_Link):
+class People_Export(ItemLink, People_Link):
     """Export people to text archive."""
     dialogTitle = _(u"Export People")
     text = _(u'Export...')
@@ -222,7 +222,7 @@ class People_Export(People_Link):
                        title=self.dialogTitle)
 
 #------------------------------------------------------------------------------
-class People_Import(People_Link):
+class People_Import(ItemLink, People_Link):
     """Import people from text archive."""
     dialogTitle = _(u"Import People")
     text = _(u'Import...')
@@ -242,13 +242,12 @@ class People_Import(People_Link):
         self.window.RefreshUI()
 
 #------------------------------------------------------------------------------
-class People_Karma(People_Link, ChoiceLink):
+class People_Karma(ChoiceLink, balt.MenuLink, People_Link):
     """Add Karma setting links."""
-    text = _(u'Karma')
     idList = ID_GROUPS
     labels = [u'%+d' % x for x in xrange(5, -6, -1)]
 
-    class _Karma(People_Link):
+    class _Karma(ItemLink, People_Link):
         def Execute(self, event):
             karma = int(self.text)
             for item in self.selected: